ba87b7712b5510c2e43ce6d461decfd2650ca2d5
Ralf <ralf.mueller@live.com>
2021-10-26 19:37:20 +0300

    Simple 2 way car

--- 2-wheel/2-wheel-car/2-wheel-car.ino
+++ 2-wheel/2-wheel-car/2-wheel-car.ino
@@ -1,41 +1,156 @@
-int motor1pin1 = 2;
-int motor1pin2 = 3;
 
-int motor2pin1 = 4;
-int motor2pin2 = 5;
+
+enum DIR {
+  BACK = -1,
+  STOP = 0,
+  FORE = 1
+};
+
+typedef struct motor {
+  int pin1;
+  int pin2;
+  int pin_speed;
+  DIR dir;      // -1 = back, 0 = stop; 1 = fore
+  int mspeed;  // 0 - 255
+} motor;
+
+#define MOTOR1_PIN1      2
+#define MOTOR1_PIN2      3
+#define MOTOR1_SPEED     9
+
+#define MOTOR2_PIN1      4
+#define MOTOR2_PIN2      5
+#define MOTOR2_SPEED     10
+
+#define MOTOR_BACK       0
+#define MOTOR_FRONT      1
+
+
+motor  all_motors[] = {
+  {
+    MOTOR1_PIN1,
+    MOTOR1_PIN2,
+    MOTOR1_SPEED,
+    STOP,
+    0
+  },
+  {
+    MOTOR2_PIN1,
+    MOTOR2_PIN2,
+    MOTOR2_SPEED,
+    STOP,
+    0
+  }
+};
+
+
+  
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(motor1pin1, OUTPUT);
-  pinMode(motor1pin2, OUTPUT);
-  pinMode(motor2pin1, OUTPUT);
-  pinMode(motor2pin2, OUTPUT);
-   pinMode(9, OUTPUT); 
+//  pinMode(motor1pin1, OUTPUT);
+//  pinMode(motor1pin2, OUTPUT);
+//  pinMode(motor2pin1, OUTPUT);
+//  pinMode(motor2pin2, OUTPUT);
+//  pinMode(9, OUTPUT); 
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  all_motor_init();
+}
 
+void all_motor_init() {
+  int i;
+  for (i = 0; i < sizeof(all_motors) / sizeof(motor); i++){
+    Serial.println("Init motor");
+    all_motors[i].dir = STOP;
+    all_motors[i].mspeed = 0;
+    Serial.println("All outputs");
+    pinMode(all_motors[i].pin1, OUTPUT);
+    pinMode(all_motors[i].pin2, OUTPUT);
+    pinMode(all_motors[i].pin_speed, OUTPUT);
+  }
+   
+}
+
+void motorCTRL(int m, DIR dir, int s)
+{
+  Serial.print(all_motors[m].pin1);
+  Serial.print(all_motors[m].pin2);
+  Serial.print(all_motors[m].pin_speed);
+  if(dir == FORE){
+    Serial.println("Motor FORE" + m);
+    digitalWrite(all_motors[m].pin1, HIGH);
+    digitalWrite(all_motors[m].pin2, LOW);
+    analogWrite(all_motors[m].pin_speed, s);
+  } else if(dir == BACK){
+    Serial.println("Motor BACK");
+    digitalWrite(all_motors[m].pin1, LOW);
+    digitalWrite(all_motors[m].pin2, HIGH);
+    analogWrite(all_motors[m].pin_speed, s);
+    all_motors[m].mspeed = s;
+  } else {
+    Serial.println("Motor STOP");
+    digitalWrite(all_motors[m].pin1, LOW);
+    digitalWrite(all_motors[m].pin2, LOW);
+    digitalWrite(all_motors[m].pin_speed, 0);
+    all_motors[m].mspeed = 0;
+  }
 }
 
 
 
 void loop() {
-  // while(1);
+//  Serial.println("Hallo fore");
+//  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+//  motorCTRL(0, FORE, 1000);
+//  delay(1000);
+//  Serial.println("Hallo stop");
+//  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+//  motorCTRL(0, STOP, 0);
+//  delay(1000);
+//  Serial.println("Hallo back");
+//  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+//  motorCTRL(0, BACK, 80);
+//  delay(1000);
+//  Serial.println("Hallo stop");
+//  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+//  motorCTRL(0, STOP, 0);
+//  delay(1000);
+
+  Serial.println("Motor speedup");
+  
+  for(int i = 100; i < 255; i++){
+    motorCTRL(0, BACK, i);
+    delay(4);
+  }
+  Serial.println("Motor speed down");
+  for(int i = 255; i >= 100; --i){
+    motorCTRL(0, BACK, i);
+    delay(4);
+  }
+  motorCTRL(0, STOP, 0);
+  
+  delay(1000);
+  
+  
   // put your main code here, to run repeatedly:   
-  analogWrite(9, 150); //ENA pin
-  digitalWrite(motor1pin1, HIGH);
-  digitalWrite(motor1pin2, LOW);
-
-  digitalWrite(motor2pin1, HIGH);
-  digitalWrite(motor2pin2, LOW);
-  delay(1000);
-
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, HIGH);
-
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, HIGH);
-  delay(1000);
-
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, LOW)
-  delay(1000);
-
+//  analogWrite(9, 150); //ENA pin
+//  digitalWrite(motor1pin1, HIGH);
+//  digitalWrite(motor1pin2, LOW);
+//
+//  digitalWrite(motor2pin1, HIGH);
+//  digitalWrite(motor2pin2, LOW);
+//  delay(1000);
+//
+//  digitalWrite(motor1pin1, LOW);
+//  digitalWrite(motor1pin2, HIGH);
+//
+//  digitalWrite(motor2pin1, LOW);
+//  digitalWrite(motor2pin2, HIGH);
+//  delay(1000);
+//
+//  digitalWrite(motor1pin1, LOW);
+//  digitalWrite(motor1pin2, LOW);
+//  delay(1000);
+//
 }
