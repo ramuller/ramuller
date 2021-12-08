f357a5e1697295f2ce0e55c25e638bc01a7ea478
ramuller_fi <ralf.mueller@live.com>
2021-12-08 19:32:18 +0200

    just some experiments

--- barcar/sw-desgin/barcarweb/barcarweb.ino
+++ barcar/sw-desgin/barcarweb/barcarweb.ino
@@ -19,60 +19,63 @@ int barCarcount = 1;
  * Login page
  */
 
-const char* loginIndex =
- "<form name='loginForm'>"
-    "<table width='20%' bgcolor='A09F9F' align='center'>"
-        "<tr>"
-            "<td colspan=2>"
-                "<center><font size=4><b>ESP32 Login Page</b></font></center>"
-                "<br>"
-            "</td>"
-            "<br>"
-            "<br>"
-        "</tr>"
-        "<tr>"
-             "<td>Username:</td>"
-             "<td><input type='text' size=25 name='userid'><br></td>"
-        "</tr>"
-        "<br>"
-        "<br>"
-        "<tr>"
-            "<td>Password:</td>"
-            "<td><input type='Password' size=25 name='pwd'><br></td>"
-            "<br>"
-            "<br>"
-        "</tr>"
-        "<tr>"
-            "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
-        "</tr>"
-        "<tr>"
-            "<td><input type='submit' onclick='start(this.form)' value='Start Barcar'></td>"
-        "</tr>"
-    "</table>"
-"</form>"
-"<script>"
-    "function check(form)"
-    "{"
-    "if(form.userid.value=='admin' && form.pwd.value=='admin')"
-    "{"
-    "window.open('/serverIndex')"
-    "}"
-    "if(form.userid.value=='run')"
-    "{"
-    "window.open('/runBarcar')"
-    "}"
-    "else"
-    "{"
-    " alert('Error Password or Username')/*displays error message*/"
-    "}"
-    "}"
-    "function start(form)"
-    "{"
-    "window.open('/runBarcar')"
-    /* " alert('No clue how to start barcar')" */
-    "}"
-"</script>";
-
+const char* loginIndex = R""""(
+ <form name='loginForm'>
+    <table width='20%' bgcolor='A09F9F' align='center'>
+        <tr>
+            <td colspan=2>
+                <center><font size=4><b>ESP32 Login Page</b></font></center>
+                <br>
+            </td>
+            <br>
+            <br>
+        </tr>
+        <tr>
+             <td>Username:</td>
+             <td><input type='text' size=25 name='userid'><br></td>
+        </tr>
+        <br>
+        <br>
+        <tr>
+            <td>Password:</td>
+            <td><input type='Password' size=25 name='pwd'><br></td>
+            <br>
+            <br>
+        </tr>
+        <tr>
+            <td><input type='submit' onclick='check(this.form)' value='Login'></td>
+        </tr>
+        <tr>
+            <td><input type='submit' onclick='start(this.form)' value='Start Barcar'></td>
+        </tr>
+        <tr>
+            <td><input type="checkbox" name='flower' value='rose' checked='checked' /></td>
+        </tr>
+    </table>
+</form>
+<script>
+    function check(form)
+    {
+    if(form.userid.value=='admin' && form.pwd.value=='admin')
+    {
+    window.open('/serverIndex')
+    }
+    if(form.userid.value=='run')
+    {
+    window.open('/runBarcar')
+    }
+    else
+    {
+     alert('Error Password or Username')/*displays error message*/
+    }
+    }
+    function start(form)
+    {
+    window.open('/runBarcar')
+    /*  alert('No clue how to start barcar') */
+    }
+</script>;
+)"""";
 
 
 
@@ -95,42 +98,43 @@ const char* runBarcar =
  * Server Index Page
  */
 
-const char* serverIndex =
-"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script>"
-"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
-   "<input type='file' name='update'>"
-        "<input type='submit' value='Update'>"
-   "</form>"
- "<div id='prg'>progress: 0%</div>"
- "<script>"
-  "$('form').submit(function(e){"
-  "e.preventDefault();"
-  "var form = $('#upload_form')[0];"
-  "var data = new FormData(form);"
-  " $.ajax({"
-  "url: '/update',"
-  "type: 'POST',"
-  "data: data,"
-  "contentType: false,"
-  "processData:false,"
-  "xhr: function() {"
-  "var xhr = new window.XMLHttpRequest();"
-  "xhr.upload.addEventListener('progress', function(evt) {"
-  "if (evt.lengthComputable) {"
-  "var per = evt.loaded / evt.total;"
-  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
-  "}"
-  "}, false);"
-  "return xhr;"
-  "},"
-  "success:function(d, s) {"
-  "console.log('success!')"
- "},"
- "error: function (a, b, c) {"
- "}"
- "});"
- "});"
- "</script>";
+const char* serverIndex = R""""(
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script>
+<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>
+   <input type='file' name='update'>
+        <input type='submit' value='Update'>
+   </form>
+ <div id='prg'>progress: 0%</div>
+ <script>
+  $('form').submit(function(e){
+  e.preventDefault();
+  var form = $('#upload_form')[0];
+  var data = new FormData(form);
+   $.ajax({
+  url: '/update',
+  type: 'POST',
+  data: data,
+  contentType: false,
+  processData:false,
+  xhr: function() {
+  var xhr = new window.XMLHttpRequest();
+  xhr.upload.addEventListener('progress', function(evt) {
+  if (evt.lengthComputable) {
+  var per = evt.loaded / evt.total;
+  $('#prg').html('progress: ' + Math.round(per*100) + '%');
+  }
+  }, false);
+  return xhr;
+  },
+  success:function(d, s) {
+  console.log('success!')
+ },
+ error: function (a, b, c) {
+ }
+ });
+ });
+ </script>;
+)"""";
 
 void barCarFunc(void);
 void barCarFunc(void) {
@@ -169,7 +173,7 @@ void setup(void) {
       delay(1000);
     }
   }
-  Serial.println("mDNS responder started");
+  Serial.println("mDNS responder started.");
   /*return index page which is stored in serverIndex */
   server.on("/", HTTP_GET, []() {
     server.sendHeader("Connection", "close");
