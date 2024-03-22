f9ee5865ae2e77ed7d628095681c1fab5f28cbfa
ramuller_fi <ralf.mueller@live.com>
2024-03-22 21:51:22 +0200

    Start with python pokemat not working

--- pokemat/pokemat.py
+++ pokemat/pokemat.py
@@ -1,13 +1,28 @@
 #!/bin/env python
 import argparse
+import time
+import os
 
 class TouchScreen:
     def __init__(self, fifoName):
         self.fifo = open(fifoName, "w")
+        # self.read = open("{}.sh".format(fifoName), "r")
+        # self.read.seek(0, 2)
+        fd = os.open("{}.sh".format(fifoName), os.O_RDWR|os.O_CREAT)
+        self.read = os.fdopen(fd, "w+")
+        
+        print("Pokemat init")
         
     def click(self, x, y, button = 1, duration = 50):
-        self.fifo.write("click:{},{},{},{}".format(x,y,button, duration))
-
+        self.fifo.write("click:{},{},{},{}\n".format(x,y,button, duration))
+        self.fifo.flush()
+        
+    def getRGB(self, x, y):
+        # print("color:{},{}\n".format(x,y))
+        self.fifo.writelines("color:{},{}\n".format(x,y))
+        self.fifo.flush()
+        time.sleep(2)
+        print(self.read.read())
         
 def main():
     parser = argparse.ArgumentParser()
@@ -15,7 +30,10 @@ def main():
     args = parser.parse_args()
     print(args)
     ts = TouchScreen(args.fifo)
-    ts.click(200,200)
+    # ts.click(200,200)
+    for y in range(200,204,1):
+        print(y)
+        ts.getRGB(200,y)
     
 if __name__ == "__main__":
     main()
